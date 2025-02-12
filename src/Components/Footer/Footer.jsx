4d52baa981701aed256cbb5106f2d3867cae227c
MOHAMED ABDIHAKIN MUHUMED <48615938+Nucmaan@users.noreply.github.com>
2025-02-13 05:30:33 +0800

    footer Update
    footer Update

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <div className=" my-2 py-1 bg-[blue] ">
     <div className="container mx-auto  py-4 mt-2 flex justify-center items-center">
-       <h1 className='text-white font-bold'>Marnol &#xa9; 2025</h1>
+       <h1 className='text-white font-bold'>Mohamed  &#xa9; 2025</h1>
     </div>
   </div>
   )
